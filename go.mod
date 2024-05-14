0a16f8b7cd0bd2bfede2c93b78a850b6c0467175
Pete at MT <peter.jones8@mineraltechnologies.com>
2024-05-14 15:20:11 +1000

    v0.0.1 - preliminary logging approach

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pjsoftware/go-logging
+
+go 1.22.1
---
+++ log.go
@@ -0,0 +1,98 @@
+package log
+
+import (
+	"fmt"
+	clog "log"
+	"os"
+	"time"
+)
+
+type Log struct {
+	open bool
+	root string
+	app  string
+	lib  string
+	file *os.File
+}
+
+const WindowsRootFolder = "C:/LogFiles"
+
+var logInstance Log
+
+func getLogInstance() *Log {
+	if logInstance.open {
+		return &logInstance
+	}
+	panic("log must be initialised via InitLog()")
+}
+
+func InitLog(root string, app string) {
+	log := &logInstance
+
+	log.root = root
+	log.app = app
+	log.lib = ""
+
+	now := time.Now()
+	folder := fmt.Sprintf("%s/%4d/%02d/%02d", root, now.Year(), now.Month(), now.Day())
+	fileName := fmt.Sprintf("%4d%02d%02d-%s.log", now.Year(), now.Month(), now.Day(), log.app)
+	os.MkdirAll(folder, 0777)
+
+	file, err := os.OpenFile(folder+"/"+fileName, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0644)
+	if err != nil {
+		return
+	}
+
+	log.file = file
+	clog.SetOutput(log.file)
+	clog.SetFlags(clog.Ldate | clog.Ltime)
+	log.open = true
+
+	Printf("Initialising '%s' log file", log.app)
+}
+
+func Close() {
+	log := getLogInstance()
+	if log.open {
+		log.file.Close()
+		log.open = false
+	}
+}
+
+func Lib(lib string) *Log {
+	log := getLogInstance()
+	log.lib = lib
+	return log
+}
+
+func (log *Log) Printf(format string, a ...any) {
+	Printf(format, a...)
+}
+
+func Printf(format string, a ...any) {
+	log := getLogInstance()
+	msg := fmt.Sprintf(format, a...)
+	prefix := log.app
+	if log.lib != "" {
+		prefix += ":" + log.lib
+		log.lib = ""
+	}
+
+	clog.Printf("[%s] %s", prefix, msg)
+}
+
+func (log *Log) Fatalf(format string, a ...any) {
+	Fatalf(format, a...)
+}
+
+func Fatalf(format string, a ...any) {
+	log := getLogInstance()
+	msg := fmt.Sprintf(format, a...)
+	prefix := log.app
+	if log.lib != "" {
+		prefix += "." + log.lib
+		log.lib = ""
+	}
+
+	clog.Fatalf("[%s] %s", prefix, msg)
+}
