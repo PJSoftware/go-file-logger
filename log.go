7b0c32bc871e2bbb1c1832958bd36ad02faba976
Peter Jones <pjsoftware@petesplace.id.au>
2025-04-16 10:52:38 +1000

    v0.0.5 - enable use of FILE_LOGGER_FOLDER env var

--- log.go
+++ log.go
@@ -8,15 +8,24 @@ import (
 	"time"
 )
 
-const root = "C:/LogFiles"
+const defaultFolder = "C:/LogFiles"
+
+func rootFolder() string {
+	folder := os.Getenv("FILE_LOGGER_FOLDER")
+	if folder != "" {
+		return folder
+	}
+
+	return defaultFolder
+}
 
 func LogWriter(app string) *os.File {
 	now := time.Now()
-	folder := fmt.Sprintf("%s/%4d/%02d/%02d", root, now.Year(), now.Month(), now.Day())
+	folder := fmt.Sprintf("%s/%4d/%02d/%02d", rootFolder(), now.Year(), now.Month(), now.Day())
 	fileName := fmt.Sprintf("%4d%02d%02d-%s.log", now.Year(), now.Month(), now.Day(), app)
 	os.MkdirAll(folder, 0777)
 
-	file, err := os.OpenFile(folder+"/"+fileName, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0644)
+	file, err := os.OpenFile(folder+"/"+fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return nil
 	}
